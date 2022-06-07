97f31a8ab0cd2e987a9a3ef886749fd74df781fd
onnyx1 <onnyx1000@gmail.com>
2022-06-07 00:55:45 -0400

    created printBoard() with unit testing

--- impossible-tic-tac-toe/src/main/java/game/Board.java
+++ impossible-tic-tac-toe/src/main/java/game/Board.java
@@ -40,13 +40,26 @@ public class Board {
 
         for (int row = 0; row < this.board.length; row++) {
             for (int column = 0; column < this.board[row].length; column++) {
-                this.board[row][column] = null;
+                this.board[row][column] = " ";
             }
         }
         return this.board;
 
     }
-    
-    
-    
+
+    public static String printBoard(String[][] board) {
+
+        String result = "   0 1 2 \n"
+                      + "   ------ \n"
+                      + " 0|" + board[0][0] + " " + board[0][1] + " " + board[0][2] + "|" + "\n"
+                      + " 1|" + board[1][0] + " " + board[1][1] + " " + board[1][2] + "|" + "\n"
+                      + " 2|" + board[2][0] + " " + board[2][1] + " " + board[2][2] + "|" + "\n"
+                      + "   ------";
+
+        System.out.println(result);
+        
+        return result;
+        
+    }
+
 }
--- impossible-tic-tac-toe/src/test/java/tests/BoardTest.java
+++ impossible-tic-tac-toe/src/test/java/tests/BoardTest.java
@@ -35,8 +35,38 @@ public class BoardTest {
     public void testCreateBoard() {
         Board instance = new Board();
         instance.createBoard();
-        String[][] expResult = {{null, null, null}, {null, null, null}, {null, null, null}};
+        String[][] expResult = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
         assertArrayEquals(instance.getBoard(), expResult);
     }
 
+    /**
+     * Test of printBoard method, of class Board.
+     */
+    @Test
+    public void testPrintBoard() {
+
+        Board x = new Board();
+        x.createBoard();
+        x.getBoard()[0][0] = "X";
+        x.getBoard()[0][1] = "O";
+        x.getBoard()[0][2] = "X";
+
+        x.getBoard()[1][0] = "X";
+        x.getBoard()[1][1] = "X";
+        x.getBoard()[1][2] = "O";
+
+        x.getBoard()[2][0] = " ";
+        x.getBoard()[2][1] = "X";
+        x.getBoard()[2][2] = "X";
+
+        String result = "   0 1 2 \n"
+                + "   ------ \n"
+                + " 0|" + "X" + " " + "O" + " " + "X" + "|" + "\n"
+                + " 1|" + "X" + " " + "X" + " " + "O" + "|" + "\n"
+                + " 2|" + " " + " " + "X" + " " + "X" + "|" + "\n"
+                + "   ------";
+        
+        assertEquals(Board.printBoard(x.getBoard()), result);
+
+    }
 }
